b42f95b497155ead1f8d1a56135a7066989bce9c
ProtectIM <web@protect.im>
2013-06-06 00:51:11 +0400

    Code reformatting

--- root/languages/ru/protectim.php
+++ root/languages/ru/protectim.php
@@ -1,27 +1,25 @@
 <?php
 
-if (!defined('IN_PHPBB'))
-{
+if (!defined('IN_PHPBB')) {
 	exit;
 }
 
-if (empty($lang) || !is_array($lang))
-{
+if (empty($lang) || !is_array($lang)) {
 	$lang = array();
 }
 
 $lang = array_merge($lang, array(
-	'CAPTCHA_PROTECTIM'					=> 'Авторизация по СМС',
+	'CAPTCHA_PROTECTIM'                 => 'Авторизация по СМС',
 
-	'PROTECTIM_SESSION'					=> 'Номер сессии',
-	'PROTECTIM_CODE'					=> 'Код авторизации',
-	'PROTECTIM_CODE_EXPLAIN'			=> 'Выслан Вам на мобильный номер',
+	'PROTECTIM_SESSION'                 => 'Номер сессии',
+	'PROTECTIM_CODE'                    => 'Код авторизации',
+	'PROTECTIM_CODE_EXPLAIN'            => 'Выслан Вам на мобильный номер',
 
-	'PROTECTIM_ERROR_UNKNOWN'			=> 'Ошибка службы СМС-авторизации',
+	'PROTECTIM_ERROR_UNKNOWN'           => 'Ошибка службы СМС-авторизации',
 
-	'PROTECTIM_ERROR_CONNECTION'		=> 'Ошибка связи с сервисом СМС-авторизации',
-	'PROTECTIM_ERROR_SESSION_INCORRECT'	=> 'Неверный номер сессии',
-	'PROTECTIM_ERROR_CODE_EMPTY'		=> 'Вы должны указать код авторизации',
-	'PROTECTIM_ERROR_CODE_INCORRECT'	=> 'Неверный код авторизации',
+	'PROTECTIM_ERROR_CONNECTION'        => 'Ошибка связи с сервисом СМС-авторизации',
+	'PROTECTIM_ERROR_SESSION_INCORRECT' => 'Неверный номер сессии',
+	'PROTECTIM_ERROR_CODE_EMPTY'        => 'Вы должны указать код авторизации',
+	'PROTECTIM_ERROR_CODE_INCORRECT'    => 'Неверный код авторизации',
 ));
 
